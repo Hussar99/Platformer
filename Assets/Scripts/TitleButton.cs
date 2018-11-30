54567cf655ab256ac88b6e47c5e390cb780696fa
Hussar99 <43067400+Hussar99@users.noreply.github.com>
2018-11-30 14:28:32 +0000

    Adding Title and the Game Over screen.

---
+++ Assets/Scripts/TitleButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour {
+
+
+    // Called when tiotle button  is clicked.
+    public void GoToTitle()
+    {
+
+        //  Return to title screen.
+        SceneManager.LoadScene("Title");
+
+    }
+
+}
